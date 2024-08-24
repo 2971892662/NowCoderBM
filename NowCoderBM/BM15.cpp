c710810f724194576734f3092552d0d11d581b60
泽鹏 李 <2971892662@qq.com>
2024-08-24 19:40:36 +0800

    BM15,16

---
+++ NowCoderBM/BM15.cpp
@@ -0,0 +1,26 @@
+//BM15 删除链表中的重复元素I
+//删除给出链表中的重复元素（链表中元素从小到大有序），使链表中的所有元素都只出现一次
+#include <stdlib.h>
+struct ListNode {
+    int val;
+    ListNode* next;
+};
+ListNode* deleteDuplicates(ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    ListNode* front = head;
+    ListNode* rear = front->next;
+    while (rear != NULL) {
+        if (front->val != rear->val) {
+            front = front->next;
+            rear = rear->next;
+            continue;
+        }
+        ListNode* p = rear;
+        rear = rear->next;
+        front->next = rear;
+        free(p);
+    }
+    return head;
+}
---
+++ NowCoderBM/BM16.cpp
@@ -0,0 +1,59 @@
+//BM16 删除链表中重复元素II
+//给出一个升序排序的链表，删除链表中的所有重复出现的元素，只保留原链表中只出现一次的元素。
+#include <stdlib.h>
+#define MaxSize 10005
+struct ListNode {
+    int val;
+    ListNode* next;
+};
+//统计每个值的出现次数
+ListNode* deleteDuplicates(ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    ListNode* front = head;
+    ListNode* rear = front->next;
+    int value[MaxSize];
+    int count[MaxSize];
+    int i = 0;
+    value[i] = front->val;
+    count[i] = 1;
+    while (rear != NULL) {
+        if (front->val != rear->val) {
+            i++;
+            value[i] = rear->val;
+            count[i] = 1;
+            front = front->next;
+            rear = rear->next;
+            continue;
+        }
+        ListNode* p = rear;
+        value[i] = front->val;
+        count[i]++;
+        rear = rear->next;
+        front->next = rear;
+        free(p);
+    }
+    int j = 0;
+    while (count[j] != 1&&j<=i) {
+        j++;
+    }
+    if (j > i) {
+        return NULL;
+    }
+    ListNode* h = (ListNode*)malloc(sizeof(ListNode));
+    h->next = NULL;
+    h->val = value[j];
+    j++;
+    ListNode* re = h;
+    for (j; j <= i; j++) {
+        if (count[j] == 1) {
+            ListNode* t = (ListNode*)malloc(sizeof(ListNode));
+            t->val = value[j];
+            t->next = NULL;
+            re->next = t;
+            re = re->next;
+        }
+    }
+    return h;
+}
