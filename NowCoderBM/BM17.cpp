d661fd70a1221d69646d255bedf2c6bff64d7c3e
泽鹏 李 <2971892662@qq.com>
2024-08-31 19:13:02 +0800

    折半查找

---
+++ NowCoderBM/BM17.cpp
@@ -0,0 +1,21 @@
+//BM17 折半查找/二分查找
+#include <vector>
+using namespace std;
+int search(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size() - 1;
+    int mid = 0;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+        if (nums[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
